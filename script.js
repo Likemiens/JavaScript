58dca1f56787abf0f7b46c79f5851cb5dcdd0c4c
Alexandr <example@gmail.com>
2020-04-25 16:59:09 +0300

    Task complete 1

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 'use strict';
 
-// let answer = +prompt("Есть ли вам 18 лет?", "");
+var money = +prompt("Ваш бюджет на месяц:", "");
+var time = prompt("Введите дату в формате YYYY-MM-DD", "2020-04-24");
 
-// console.log(typeof(answer));
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
 
-let incr = 10,
-    decr = 10;
+let firstQuestion = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    secondQuestion = prompt("Во сколько обойдется?", ""),
+    firstQuestionCopy = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    secondQuestionCopy = prompt("Во сколько обойдется?", "");
 
-console.log(++incr + "," + --decr);
+appData.expenses.firstQuestion = secondQuestion;
+appData.expenses.firstQuestionCopy = secondQuestionCopy;
+
+alert(appData.budget / 30);
